ef5f56d688e9811859edb16689e2a2b68ab00d25
rangerRussel <coronadorussel_bscs@plmun.edu.ph>
2025-02-24 11:08:42 +0800

    Submitting OBJPROG Week 05 - Laboratory # 17

---
+++ GradeCalculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+	public class GradeCalculator {
+		public static String calculateGrade(float score) {
+
+		if (score >= 90) {
+		return "A";
+		}
+		
+		else if (score >= 80) {
+		return "B";
+		}
+		
+		else if (score >= 70) {
+		return "C";
+		} 
+		
+		else if (score >=60) {
+	   	return "D";
+		} 
+		
+		else{
+		return "F";
+		}
+}
+
+	public static void main(String[] args) {
+	  Scanner scanner = new Scanner(System.in);
+
+	System.out.print("Enter your grade: ");
+	float getStudentScore = scanner.nextFloat();
+
+	String result = calculateGrade(getStudentScore);
+
+	System.out.println(getStudentScore + " corresponds to grade: " + result);
+scanner.close();
+	}
+}
